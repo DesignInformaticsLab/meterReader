b6c205d68adc72d4a557dfe1072518d9c2b0f4a9
malcolmregan <mjregan1@asu.edu>
2017-04-14 17:05:05 -0500

    removetest

--- public/javascripts/main_malcolm.js
+++ public/javascripts/main_malcolm.js
@@ -90,7 +90,7 @@ $(document).ready(function() {
             }
         });
     }
-    test();
+    //test();
 
 // read and send digits
 //var img_address = "https://upload.wikimedia.org/wikipedia/en/8/82/Water_meter_register.jpg";
