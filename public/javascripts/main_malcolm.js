8584692934cd39f7fa59b1442f2f62826718572d
malcolmregan <mjregan1@asu.edu>
2017-04-14 16:39:41 -0500

    asfsfasf

--- public/javascripts/main_malcolm.js
+++ public/javascripts/main_malcolm.js
@@ -57,7 +57,7 @@ $(document).ready(function() {
 
     function test(){
         // specify image address
-        var address = 'https://raw.githubusercontent.com/DesignInformaticsLab/meterReader/master/data/number.png';
+        var address = 'https://raw.githubusercontent.com/DesignInformaticsLab/meterReader/master/data/2.png';
         //var address = 82;
 
         //var img = new Image();
@@ -133,7 +133,7 @@ $(document).ready(function() {
                 console.error(err);
             });
     }
-    test();
+    //test();
 });
 
 
