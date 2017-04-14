8737aebc5ed2f18275a0f635ab31c3d8648f9158
malcolmregan <mjregan1@asu.edu>
2017-04-13 23:58:14 -0500

    wqeqweqweq

--- public/javascripts/main_malcolm.js
+++ public/javascripts/main_malcolm.js
@@ -34,8 +34,8 @@ $(document).ready(function() {
         canvas.height = height;
         context.drawImage(video, 0, 0, width, height);
         var data = canvas.toDataURL('image/png');
+        read_img('https://raw.githubusercontent.com/DesignInformaticsLab/meterReader/master/data/2.png');
         //read_img(data);
-        test();
     }, false);
 
     function train(){
