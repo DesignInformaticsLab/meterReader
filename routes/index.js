7c1461a9f20570b7603547f94e48a5277364062d
malcolmregan <mjregan1@asu.edu>
2017-04-16 18:03:57 -0500

    Test main
    2232

--- routes/index.js
+++ routes/index.js
@@ -55,19 +55,39 @@ router.post('/read', function (req, res) {
     net.layers[7].biases = layers[7].biases;
     net.layers[7].filters = layers[7].filters;
 
-    var image = req.body['image'];
     x = new convnetjs.Vol(28, 28, 1, 0.0);
+    var address = "./data/digit.png";
+    // note: getpixels reads row by row, not column by column!
+    getPixels(address, function (err, data) {
+        var image = data.data;
 
-    var W = 28 * 28;
-    for (var i = 0; i < W; i++) {
-        var ix = i * 4;
-        x.w[i] = image[ix] / 255.0;
-    }
-    x = convnetjs.augment(x, 28, 1, 1);
+        var W = 28 * 28;
+        for (var i = 0; i < W; i++) {
+            var ix = i * 4;
+            x.w[i] = image[ix] / 255.0;
+        }
 
-    var output_probabilities_vol = net.forward(x);
+        var output_probabilities_vol = net.forward(x);
 
-    res.send({'prob': output_probabilities_vol, 'id': req.body['id']});
+        res.send({'prob': output_probabilities_vol, 'id': req.body['id']});
+    });
+
+    //UNCOMMENT FROM HERE
+    //var image = req.body['image'];
+    //x = new convnetjs.Vol(28, 28, 1, 0.0);
+
+    //var W = 28 * 28;
+    //for (var i = 0; i < W; i++) {
+    //    var ix = i * 4;
+    //    x.w[i] = image[ix] / 255.0;
+    //}
+    //x = convnetjs.augment(x, 28, 1, 1);
+
+    //var output_probabilities_vol = net.forward(x);
+
+    //res.send({'prob': output_probabilities_vol, 'id': req.body['id']});
+    //UNCOMMENT TO HERE
+
 
     //pg.connect(connection, function(err, client, done) {
     //  client.query('SELECT model FROM readmeter_model_table LIMIT 1', function(err, result) {
