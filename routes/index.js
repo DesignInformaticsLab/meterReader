a000a6ad15395e2b357b816d61742f9120aebec6
malcolmregan <mjregan1@asu.edu>
2017-04-17 22:40:10 -0500

    test CPPN 0 on main_MNIST

--- routes/index.js
+++ routes/index.js
@@ -55,19 +55,29 @@ router.post('/read', function (req, res) {
     net.layers[7].biases = layers[7].biases;
     net.layers[7].filters = layers[7].filters;
 
-    var image = req.body['image'];
+    //var image = req.body['image'];
+    //x = new convnetjs.Vol(28, 28, 1, 0.0);
+    //var W = 28 * 28;
+    //for (var i = 0; i < W; i++) {
+    //    var ix = i * 4;
+    //    x.w[i] = image[ix] / 255.0;
+    //}
+    //x = convnetjs.augment(x, 28, 1, 1);
+    //var output_probabilities_vol = net.forward(x);
+    //res.send({'prob': output_probabilities_vol, 'id': req.body['id']});
+
     x = new convnetjs.Vol(28, 28, 1, 0.0);
-
-    var W = 28 * 28;
-    for (var i = 0; i < W; i++) {
-        var ix = i * 4;
-        x.w[i] = image[ix] / 255.0;
-    }
-    x = convnetjs.augment(x, 28, 1, 1);
-
-    var output_probabilities_vol = net.forward(x);
-
-    res.send({'prob': output_probabilities_vol, 'id': req.body['id']});
+    var address = "./data/CPPN0.png";
+    getPixels(address, function (err, data) {
+        var image = data.data;
+        var W = 28 * 28;
+        for (var i = 0; i < W; i++) {
+            var ix = i * 4;
+            x.w[i] = image[ix] / 255.0;
+        }
+        var output_probabilities_vol = net.forward(x);
+        res.send({'prob': output_probabilities_vol, 'id': req.body['id']});
+    });
 
 
     //pg.connect(connection, function(err, client, done) {
