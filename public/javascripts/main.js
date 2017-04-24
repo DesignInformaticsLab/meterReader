e18e60ce1d3bcfe61d69de6bdfb9a2614b474203
vaibhavjhawar <vaibhav.jhawar@asu.edu>
2017-04-23 18:26:24 -0700

    final test 3

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -95,12 +95,12 @@ $(document).ready(function() {
 //var img_address = "https://upload.wikimedia.org/wikipedia/en/8/82/Water_meter_register.jpg";
     var crop_height = 150;
     var crop_width = 150;
-    var crop_top = 400;
+    var crop_top = 600;
     var crop_left = 250;
     var move = 200;
     $("#frame").css("top",crop_top);
     $("#frame").css("left",crop_left);
-    $("#frame").css("width",crop_width + move*1);
+    $("#frame").css("width",crop_width );
     $("#frame").css("height",crop_height);
 
     function read_img(img_address){
