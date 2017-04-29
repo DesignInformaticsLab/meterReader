224bd88350366919f2277cf0ec3f7441668425ff
vaibhavjhawar <vaibhav.jhawar@asu.edu>
2017-04-29 14:23:44 -0700

    New final test 24

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -111,9 +111,10 @@ $(document).ready(function() {
 
     function read_img(img_address){
         var canvas_small = [];
-        for (var iter=0;iter<num_img;iter++){
-            canvas_small.push(document.getElementById("#canvas"+(parseInt(iter+1))));
-        }
+        //for (var iter=0;iter<num_img;iter++){
+            canvas_small.push(document.getElementById('canvas1'));
+            canvas_small.push(document.getElementById('canvas2'));
+        //}
 
 
         Jimp.read(img_address).then(
