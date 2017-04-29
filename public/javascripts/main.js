15c1110ecc829ef74cb4e07e495a8c153f8c2b10
vaibhavjhawar <vaibhav.jhawar@asu.edu>
2017-04-29 11:58:36 -0700

    New final test 6

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
     var startbutton = document.getElementById('startbutton');
     var num_img = 1;
     var digit = Array(num_img);
-    var width = 300;
-    var height = 300;
+    var width = 28;
+    var height = 28;
 
     navigator.mediaDevices.getUserMedia(constraints)
         .then(function (mediaStream) {
