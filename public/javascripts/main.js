97b7a15fa047b36d2ae2469f8a04871ed10d456e
vaibhavjhawar <vaibhav.jhawar@asu.edu>
2017-05-06 11:47:08 -0700

    final 1

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -123,10 +123,22 @@ $(document).ready(function() {
         Jimp.read(src).then(function (lenna) {
             var small_image = lenna.quality(100)
                 .greyscale();
+            if (iter == 0){
+                small_image = small_image.crop( 120, 245, 50, 60);
+            }
             if (iter == 1){
                 small_image = small_image.crop( 100, 245, 40, 60);
             }
-            if (iter == 0){
+            if (iter == 2){
+                small_image = small_image.crop( 120, 245, 50, 60);
+            }
+            if (iter == 3){
+                small_image = small_image.crop( 120, 245, 50, 60);
+            }
+            if (iter == 4){
+                small_image = small_image.crop( 120, 245, 50, 60);
+            }
+            if (iter == 5){
                 small_image = small_image.crop( 120, 245, 50, 60);
             }
             //.crop( crop_left+move*iter, crop_top, crop_width, crop_height)
